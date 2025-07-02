f55adbd61890b17cc851ebace648cdfdda91c479
Lourdes González <l.gonzalezte@gmail.com>
2025-07-02 12:16:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,305 @@
+<?php 
+include ("header.html");
+?>
+
+<main class="index-main">
+<!-- Topic Cards -->
+<div id="cards_landscape_wrap-2">
+        <div class="container">
+            <div class="row">
+            <!-- <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>Title 01</h6>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div> -->
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="catllejeros/index.php">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>CATLLEJEROS</h6>
+                                    <p>Página web para protectora de animales creada con PHP, MySQL, HTML, Bootstrap y CSS.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div> 
+                <!-- <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="examen3\index.php">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>EXAMEN 3</h6>
+                                    <p>Programa básico creado con PHP para calcular poderes y daño inflingido en función del tipo de habilidades y nivel que indiquemos. También muestra la imagen en función del tipo de habilidad seleccionada</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>  -->
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="examen3\index.php">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>EXAMEN 3</h6>
+                                    <p>Programa básico creado con PHP  que contiene registro, login, acceso a perfil privado con codificación de contraseña y comprobación de registro</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div> 
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="examen2\index.php">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>EXAMEN 2</h6>
+                                    <p>Programa básico creado con PHP para calcular poderes y daño inflingido en función del tipo de habilidades y nivel que indiquemos. También muestra la imagen en función del tipo de habilidad seleccionada</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div> 
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="restaurante\restaurante.php">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>RESTAURANTE</h6>
+                                    <p>Programa básico creado con PHP calcular los ingresos de un restaurante en función de los menús vendidos y las propinas recibidas</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div> 
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="arraymultidimensional2\arrayMulti.php">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>FRASE DEL DÍA</h6>
+                                    <p>Programa básico creado con PHP para frases de forma aleatoria empleando array multidimensional</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div> 
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="citasAleatorias/advices.php">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>GALLETAS DE LA FORTUNA</h6>
+                                    <p>Programa básico creado con PHP para mostrar citas y números de forma aleatoria</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div> 
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="programaTrabajador/" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>PROGRAMA TRABAJADOR</h6>
+                                    <p>Programa realizado con PHP para mostrar datos y calcular sueldo en función del tipo de empleado, horas trabajadas, etc.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="programaTrabajadorBasico/" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>PROGRAMA TRABAJADOR BÁSICO</h6>
+                                    <p>Programa realizado con PHP para mostrar datos básicos en función del tipo de empleado</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="programaAutonomoPhp/" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>PROGRAMA AUTÓNOMO</h6>
+                                    <p>Programa realizado con PHP para calcular salario en función de distintas categorías de empleados </p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="programaPhp/" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>PROGRAMA PHP</h6>
+                                    <p>Calculadora para realizar suma de dos valores</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="moviesApp/" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>MOVIES APP</h6>
+                                    <p>Aplicación realizada con PHP y MySQL que permite registrar películas, editarlas y borrarlas. Contiene buscador y varias páginas con diferentes vistas. También incluye la posibilidad de registrarse y acceder mediante login en la web.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="voiceAPP/public/index.html" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>VOICE APP</h6>
+                                    <p>Aplicación realizada que reconoce un mensaje de voz, lo transcribe y lo envía a openai y envía la respuesta. Realizada con liberías de npm como  multer para la gestión de los archivos de audio,  whisper para transcribir, socket para permitir comunicación bidireccional, file system para leer el archivo de audio. Para arrancar la app necesitas una api key de openai.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="crud-imagenes/" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>CRUD Imágenes</h6>
+                                    <p>Crud de imágenes realizado con PHP y MySQL</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="crud-peliculas/" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>CRUD Películas</h6>
+                                    <p>CRUD de películas realizado con PHP y MySQL</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="mi_primer_crud" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">                                    
+                                    <h6>CRUD</h6>
+                                    <p>CRUD básico creado con PHP</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="login-form.php" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>FORMULARIO ACCESO</h6>
+                                   <p>Formulario de acceso realizado con PHP</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="contacto.php" target="_blank">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                <img src="images/pc.png" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h6>FORMULARIO REGISTRO</h6>
+                                   <p>Formulario de registro realizado con php</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+
+
+<?php 
+include ("footer.html");
+?>
